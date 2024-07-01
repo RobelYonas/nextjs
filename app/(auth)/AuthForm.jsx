3e7db8fb9869012ed66a244b98099f2009df14ec
RobelYonas <robel_yonas.tesfaye_bekele0007@stud.hkr.se>
2024-07-01 22:56:31 +0200

    I have add comment in order to help to understand code when review code

--- app/(auth)/AuthForm.jsx
+++ app/(auth)/AuthForm.jsx
@@ -2,12 +2,20 @@
 
 import { useState } from 'react'
 
+/**
+ * Renders an authentication form with email and password inputs and a submit button.
+ * Calls the handleSubmit function when the form is submitted with the email and password values.
+ * @param {function} handleSubmit - The function to be called when the form is submitted.
+ * @returns {JSX.Element} - The rendered authentication form.
+ */
 export default function AuthForm({ handleSubmit }) {
+    // State variables for email and password inputs
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     return (
         <form onSubmit={(e) => handleSubmit(e, email, password)}>
+            {/* Email input */}
             <label>
                 <span>Email:</span>
                 <input
@@ -17,6 +25,7 @@ export default function AuthForm({ handleSubmit }) {
                     required
                 />
             </label>
+            {/* Password input */}
             <label>
                 <span>Password:</span>
                 <input
@@ -26,6 +35,7 @@ export default function AuthForm({ handleSubmit }) {
                     required
                 />
             </label>
+            {/* Submit button */}
             <button className="btn-primary">Submit</button>
         </form>
     )
